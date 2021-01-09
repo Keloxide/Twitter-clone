797aa8bd9c237b21152997c81f4c54b39498e3d1
Kellie <76688880+Keloxide@users.noreply.github.com>
2021-01-09 14:28:08 +0100

    Add files via upload

---
+++ TWITTER PROJECT/twitter.js
@@ -0,0 +1,40 @@
+/*
+alert("Hello, Welcome to 2021!!");
+
+var answer2 = confirm("would you like to continue this session?");
+
+if(answer2 === true ){
+    alert("Great choice");
+    var answer = prompt("what month of the year are we in?").toLowerCase();
+if (answer === "january" ) {
+    alert("January is the very first month of the year.");
+}else if (answer === "february" ){
+     alert("February, the month of love");
+}else if (answer === "march" ){
+     alert("3rd month of the year, March has its own memories");
+}else if (answer === "april" ){
+     alert("You are correct, April Fools day, LOL");
+}else if (answer === "may" ){
+     alert("May is actually my Birth month, it takes up number 5 position");
+}else if (answer === "june" ){
+     alert("June! June!! June!!!... the 6th month");
+}else if (answer === "july" ){
+     alert("Oh its a rainy month, July takes the 7th spot on the calender!");
+}else if (answer === "august" ){
+     alert("Hmmmmm... August, its got its own memories.");
+}else if (answer === "september" ){
+     alert("September to remember!!!!");
+}else if (answer === "october" ){
+     alert("October the 10th month, my mother was born in this month!");
+}else if (answer === "november" ){
+     alert("November is the Eleventh month of the year");
+}else if (answer === "december" ){
+     alert("This is the last month of the year,Welcome to December... its Christmas Baby!!");
+} else{
+    alert("Please enter a valid Month");
+     }
+}
+else{
+    alert("Okay, Thanks for your time.");
+}
+*/
